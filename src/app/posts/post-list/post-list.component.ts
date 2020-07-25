1a0aa132b3466336825fb1d79c1aefbd3c6c0027
David Armendariz <darmendariz1998@outlook.com>
2020-07-24 21:16:20 -0500

    Creating posts with property and event binding

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
@@ -10,5 +10,5 @@ export class PostListComponent {
   //   { title: 'Second Post', content: "This is the second post's content" },
   //   { title: 'Third Post', content: "This is the third post's content" },
   // ];
-  posts = [];
+  @Input() posts = [];
 }
