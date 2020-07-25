27d1a29426001a9705469f74417faaec788da52d
David Armendariz <darmendariz1998@outlook.com>
2020-07-25 09:25:30 -0500

    Added dependency injection

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,3 +1,4 @@
+import { PostsService } from './../posts.service';
 import { Component, Input } from '@angular/core';
 import { Post } from '../post.model';
 @Component({
@@ -6,10 +7,6 @@ import { Post } from '../post.model';
   styleUrls: ['./post-list.component.css'],
 })
 export class PostListComponent {
-  // posts = [
-  //   { title: 'First Post', content: "This is the first post's content" },
-  //   { title: 'Second Post', content: "This is the second post's content" },
-  //   { title: 'Third Post', content: "This is the third post's content" },
-  // ];
   @Input() posts: Post[] = [];
+  constructor(public postsService: PostsService) {}
 }
